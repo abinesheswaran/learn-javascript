7cfde64a4ff17d32239c3d107cd87c158c7c221a
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-11-14 06:39:28 +0530

    start this

--- counter.js
+++ counter.js
@@ -7,3 +7,18 @@ export function setupCounter(element) {
   element.addEventListener('click', () => setCounter(counter + 1));
   setCounter(0);
 }
+
+const obj = {
+  name: 'abinesh',
+  fn: function () {
+    console.log(this);
+  },
+};
+const obj2 = {
+  name: 'anushick',
+};
+obj2.fn = obj.fn;
+obj.fn();
+obj2.fn();
+const fn = obj.fn;
+fn();
