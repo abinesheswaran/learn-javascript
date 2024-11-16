26fafa2cb6b0260968a1b900b03ed86d8376b54b
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-11-16 10:58:53 +0530

    done

--- counter.js
+++ counter.js
@@ -1,23 +1,36 @@
 class Employee {
   firstName = 'abinesh';
   age = 25;
-  setName = function (nameVal) {
+  gender = 'Male';
+  setName(nameVal) {
     this.firstName = nameVal;
     this.getAll();
-  };
+  }
   setAge = (ageVal) => {
     this.age = ageVal;
     this.getAll();
   };
   getAll = function () {
-    console.log(this.firstName, this.age);
+    console.log(this.firstName, this.age, this.gender, this.location);
   };
 }
+
+Employee.prototype.location = 'Bangalore';
+Employee.prototype.setLocation = function (val) {
+  this.location = val;
+  this.getAll();
+};
+Employee.prototype.setGender = function (val) {
+  this.gender = val;
+  this.getAll();
+};
 
 const abinesh = new Employee();
 abinesh.getAll();
 abinesh.setName('Abinesh E');
 abinesh.setAge(26);
+abinesh.setGender('Angry Male');
+abinesh.setLocation('Brookfield Bangalore');
 abinesh.firstName = 'NaN';
 abinesh.getAll();
 
