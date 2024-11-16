e0fc1e69c4c1a77cc44952da757cd3e64cd33840
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-11-16 10:49:09 +0530

    done

--- counter.js
+++ counter.js
@@ -1,9 +1,67 @@
-export function setupCounter(element) {
-  let counter = 0;
-  const setCounter = (count) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
-  };
-  element.addEventListener('click', () => setCounter(counter + 1));
-  setCounter(0);
+class Human {
+  firstName = 'NaN';
+  age = 0;
+  constructor(age, name) {
+    this.firstName = name;
+    this.age = age;
+  }
+  sayHello() {
+    console.log('Hello...- my name is ' + this.firstName);
+  }
+  run() {
+    if (this.age > 25)
+      console.log('I am ' + this.age + ' years old... i cant run');
+    else console.log(this.firstName + ' started running...');
+  }
 }
+
+class Employee extends Human {
+  firstName = 'abinesh';
+  age = 25;
+  gender = 'Male';
+  constructor() {
+    super(25, 'abinesh');
+  }
+  setName = function (nameVal) {
+    this.firstName = nameVal;
+    this.getAll();
+  };
+  setAge = (ageVal) => {
+    this.age = ageVal;
+    this.getAll();
+  };
+  getAll = function () {
+    console.log(this.firstName, this.age, this.gender);
+  };
+
+  //Correct getters and setters
+  get gender() {
+    return this.gender;
+  }
+  set genderVal(val) {
+    this.gender = val + '.';
+    this.getAll();
+  }
+
+  //Static methods
+  static sayHi() {
+    console.log(this);
+    console.log('Hi...!!!');
+  }
+}
+
+const abinesh = new Employee();
+abinesh.getAll();
+abinesh.setName('Abinesh E');
+abinesh.setAge(26);
+abinesh.firstName = 'NaN';
+abinesh.getAll();
+console.log(abinesh.gender);
+abinesh.genderVal = 'Arogent Male';
+console.log(abinesh);
+Employee.sayHi();
+abinesh.setName('Abinesh E');
+abinesh.sayHello();
+abinesh.run();
+
+export function setupCounter() {}
