4c11b261bee99c78ef35a439b7d8840e2febc5ac
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-11-16 10:49:09 +0530

    done

--- counter.js
+++ counter.js
@@ -1,9 +1,44 @@
-export function setupCounter(element) {
-  let counter = 0;
-  const setCounter = (count) => {
-    counter = count;
-    element.innerHTML = `count is ${counter}`;
+class Employee {
+  firstName = 'abinesh';
+  age = 25;
+  gender = 'Male';
+  setName = function (nameVal) {
+    this.firstName = nameVal;
+    this.getAll();
   };
-  element.addEventListener('click', () => setCounter(counter + 1));
-  setCounter(0);
+  setAge = (ageVal) => {
+    this.age = ageVal;
+    this.getAll();
+  };
+  getAll = function () {
+    console.log(this.firstName, this.age, this.gender);
+  };
+
+  //Correct getters and setters
+  get gender() {
+    return this.gender;
+  }
+  set genderVal(val) {
+    this.gender = val + '.';
+    this.getAll();
+  }
+
+  //Static methods
+  static sayHi() {
+    console.log(this);
+    console.log('Hi...!!!');
+  }
 }
+
+const abinesh = new Employee();
+abinesh.getAll();
+abinesh.setName('Abinesh E');
+abinesh.setAge(26);
+abinesh.firstName = 'NaN';
+abinesh.getAll();
+console.log(abinesh.gender);
+abinesh.genderVal = 'Arogent Male';
+console.log(abinesh);
+Employee.sayHi();
+
+export function setupCounter() {}
