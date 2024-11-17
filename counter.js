2bb9cfd50d0e04d8d49a8752252835ea7d2dfa55
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-11-17 21:34:00 +0530

    done

--- counter.js
+++ counter.js
@@ -7,3 +7,18 @@ export function setupCounter(element) {
   element.addEventListener('click', () => setCounter(counter + 1));
   setCounter(0);
 }
+
+function sample(i) {
+  if (i < 6) throw Error('This is sample error ***!!! ' + i);
+  else return i;
+}
+
+setInterval(() => {
+  try {
+    const i = Math.round(Math.random() * 10);
+    const n = sample(i);
+    console.log(n);
+  } catch (err) {
+    console.log(err);
+  }
+}, 500);
